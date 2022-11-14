cf1fa57bd44419ff128960a9b1e92f946b604af7
Matan Mayerowicz <matan417@gmail.com>
2022-11-14 13:03:44 +0200

    adding description
    adding discription for example class

--- src/example.java
+++ src/example.java
@@ -1,9 +1,10 @@
-
+/*This class is for example git hub and testing */
 public class example {
 
 	public static void main(String[] args) 
 	{
 		System.out.println("testing");
+		System.out.println("Matan");
 		// TODO Auto-generated method stub
 		System.out.println("Liad");
 		System.out.println("Roei");
