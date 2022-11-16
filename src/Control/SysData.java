28584c117f791ed80fc4221e50058fe2079faf8a
matanmay <matan417@gmail.com>
2022-11-16 15:15:45 +0200

    update getQuestions and sections on sysdata class
    Section of game history and section of Question.
    game history will be a JSON
    question will be a JSON according to Module format

--- src/Control/SysData.java
+++ src/Control/SysData.java
@@ -1,8 +1,10 @@
 package Control;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
@@ -54,6 +56,8 @@ public class SysData implements Initializable
 
 	}
 	
+	// Questions Zone // 
+	
 	/*
 	 * This method do import JSON to Question array
 	 */
@@ -81,11 +85,74 @@ public class SysData implements Initializable
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-
 	}
 
+	/**
+	 * This method is used for fetching the questions
+	 * 
+	 * @return List of questions
+	 */
+	public List<Question> getQuestions()
+	{
+		if (questions == null)
+			try
+			{
+				loadQuestionsDetails();
+			} catch (IOException | ParseException e)
+			{
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		return questions;
+	}
 	
-
+	/**
+	 * Updating JSON of question
+	 * 
+	 * @param questionsToUpdate list to update json
+	 * 
+	 * TO-DO method after question class created
+	 */
+	public static void updateJson(List<Question> questionsToUpdate)
+	{
+		//TO DO//
+	}
+	
+	
+	
+	// Game Zone // 
+	
+	/*
+	 * This method get File
+	 * This method is save the game to JSON File
+	 */
+	public void saveGameToFile(File file)
+	{
+		try
+		{
+			PrintWriter writer;
+			writer = new PrintWriter(file);
+			//writer.println(game.getSavedGameString()); //To do in game class//  // 
+			writer.close();
+		} catch (IOException ex)
+		{
+			ex.printStackTrace();
+		}
+	}
+	
+	/*
+	 * This method is load games from JSON File
+	 * 
+	 * TO DO after create game java class
+	 */
+	public void loadGamesHistory()
+	{
+		//TO DO - after game class create
+	}
+	
+	
+	
+	
 	
 	
 
