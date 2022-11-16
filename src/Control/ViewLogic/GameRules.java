8746f043395fc323e39db76d3d0c8ecc457d9c50
Liad Uziel <liad.uz@gmail.com>
2022-11-16 22:13:24 +0200

    Creating game rules screen

---
+++ src/Control/ViewLogic/GameRules.java
@@ -0,0 +1,26 @@
+package Control.ViewLogic;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class GameRules {
+	
+    @FXML
+    private AnchorPane mainPane;
+    
+
+    @FXML
+    void returnToHomePage(ActionEvent event) throws IOException {
+		Parent newRoot = FXMLLoader.load(getClass().getResource("/View/HomePage.fxml"));
+		Stage primaryStage = (Stage) mainPane.getScene().getWindow();
+		primaryStage.getScene().setRoot(newRoot);
+		primaryStage.setTitle("Knight's Move");
+		primaryStage.show();    }
+
+}
