e58e941f0321e49d8d2bc2a0701e4ef00322abdf
Liad Uziel <liad.uz@gmail.com>
2022-11-16 22:12:36 +0200

    Creating the home page screen

---
+++ src/Control/ViewLogic/HomePage.java
@@ -0,0 +1,46 @@
+package Control.ViewLogic;
+
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class HomePage {	
+
+    @FXML
+    private AnchorPane mainPane;
+	
+
+    @FXML
+    void onGameHistory(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onGameRules(ActionEvent event) throws IOException {
+		Parent newRoot = FXMLLoader.load(getClass().getResource("/View/GameRules.fxml"));
+		Stage primaryStage = (Stage) mainPane.getScene().getWindow();
+		primaryStage.getScene().setRoot(newRoot);
+		primaryStage.setTitle("Rules");
+		primaryStage.show();
+    }
+
+    @FXML
+    void onQuestionsManagement(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onStartGame(ActionEvent event) throws IOException {
+		Parent newRoot = FXMLLoader.load(getClass().getResource("/View/InsertNickname.fxml"));
+		Stage primaryStage = (Stage) mainPane.getScene().getWindow();
+		primaryStage.getScene().setRoot(newRoot);
+		primaryStage.setTitle("Insert Nickname");
+		primaryStage.show();
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,16 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 
-public class RunFXML extends Application {
+public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("/View/demo.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/View/HomePage.fxml"));
 			Scene scene = new Scene(root);
 //			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
+			primaryStage.setTitle("Knight's Move");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
