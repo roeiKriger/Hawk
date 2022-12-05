8cf98a17bfd132f6e291a0a24c308666e2c3f2ad
nogadm <nogadm@gmail.com>
2022-12-05 15:47:42 +0200

    updated move pattern in King

--- src/Model/King.java
+++ src/Model/King.java
@@ -16,20 +16,35 @@ public class King extends Piece{
 
 	}
 
+
+	//possible row moves according to king rules
 	@Override
-	public Square[][] move(int level) {
+	int[] calculateRows(int level) {	
+		int currRow = this.getRow();
+		int possibleNewRow[]={currRow-1,currRow-1,currRow-1,currRow+1,currRow+1,currRow+1,currRow,currRow};
+		return possibleNewRow;
+	}
+	
+	//possible column moves according to king rules
+	@Override
+	int[] calculateCols(int level) {
+		int currCol = this.getCol();
+		int possibleNewCol[]={currCol-1,currCol,currCol+1,currCol-1,currCol,currCol+1,currCol-1,currCol+1};
+		return possibleNewCol;
+	}
+	
+	
+	//create a set of all possible squares the king can step on
+	@Override
+	Square[][] pairAllPossibleMoves(int[] possibleNewRow, int[] possibleNewCol){
 		Square[][] possibleMoves = new Square[8][8];
 		for (int i=0;i<8;i++) {
 			for (int j=0; j<8; j++) {
 				possibleMoves[i][j] = new Square("noColor", false);
 			}
 		}
-		int currRow = this.getRow();
-		int currCol = this.getCol();
-		
-		//King can move only one step each turn. So all the possible 8 Squares have been considered
-		int possibleNewRow[]={currRow-1,currRow-1,currRow-1,currRow+1,currRow+1,currRow+1,currRow,currRow};
-		int possibleNewCol[]={currCol-1,currCol,currCol+1,currCol-1,currCol,currCol+1,currCol-1,currCol+1};
+		//now we have two arrays, each of them has locations of the placements, first array
+		//is the Row and the second is the Column, we will match them to create a list of all possible squares
 		for(int i=0;i<8;i++){
 			if((possibleNewRow[i] >= 0 && possibleNewRow[i] <8 && possibleNewCol[i] >=0 && possibleNewCol[i] <8))
 				possibleMoves[possibleNewRow[i]][possibleNewCol[i]].setCanVisit(true);			
@@ -37,8 +52,6 @@ public class King extends Piece{
 		
 		return possibleMoves;
 	}
-
-	
 	
 	public int getSpeed() {
 		return speed;
