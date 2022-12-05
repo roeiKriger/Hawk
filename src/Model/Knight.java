3ea5eab191543bf1d79ad6b54948eccf97323828
nogadm <nogadm@gmail.com>
2022-12-05 15:47:55 +0200

    updated move pattern in Knight

--- src/Model/Knight.java
+++ src/Model/Knight.java
@@ -4,41 +4,76 @@ public class Knight extends Piece{
 
 	public Knight(int row, int col) 
 	{
-		super(row, col);
-	
+		super(row, col);	
 	}
 
-	/*
-	 * Method will calculate the possible moves for knight, per given level.
-	 * The method will return a board of possible moves, which the type of the possible squares will be "possible".
-	 */
 
+	//possible row moves according to knight rules
 	@Override
-	public Square[][] move(int level) {
+	int[] calculateRows(int level) {
+		int currentRow = getRow();
+		int possibleNewRow[];
+		
+		//possible row moves according to level 2 rules
+		//two squares straight then one diagonal or two squares diagonal then one straight
+		if(level==2) {
+			possibleNewRow = new int[] {currentRow-2,currentRow-1,currentRow+1,currentRow+2,currentRow-3,currentRow+3,
+				currentRow-3,currentRow+3,currentRow-3,currentRow+3,currentRow-3,currentRow+3,
+				currentRow-2,currentRow-1,currentRow+1,currentRow+2};
+			
+		}
+		//possible row moves according to chess regular rules
+		else {
+			possibleNewRow = new int[] {currentRow-1,currentRow+1,currentRow-2,currentRow+2,currentRow-2,
+					currentRow+2,currentRow-1,currentRow+1};
+		}
+		
+		return possibleNewRow;
+
+	}
+
+	//possible column moves according to knight rules
+	@Override
+	int[] calculateCols(int level) {
+		int currentCol = getCol();
+		int possibleNewCol[];
+		
+		//possible column moves according to level 2 rules
+		//two squares straight then one diagonal or two squares diagonal then one straight
+		if(level==2) {
+			possibleNewCol = new int[] {currentCol-3,currentCol-3,currentCol-3,currentCol-3,currentCol-2,currentCol-2,
+				currentCol-1,currentCol-1,currentCol+1,currentCol+1,currentCol+2,currentCol+2,
+				currentCol+3,currentCol+3,currentCol+3,currentCol+3};
+			
+		}
+		//possible column moves according to chess regular rules
+		else {
+			possibleNewCol = new int[] {currentCol-2,currentCol-2,currentCol-1,currentCol-1,currentCol+1,
+					currentCol+1,currentCol+2,currentCol+2};
+		}
+		
+		return possibleNewCol;
+	}
+	
+	
+	//create a set of all possible squares the knight can step on
+	@Override
+	Square[][] pairAllPossibleMoves(int[] possibleNewRow, int[] possibleNewCol) {
 		// initiate variables
 		Square[][] possibleMoves = new Square[8][8];
-		int possibleNewRow[] = new int[8];
-		int possibleNewCol[] = new int[8];
-		int numberOfPossibleSquares = Constants.SQUARES_DEFAULT;
-
-		// when the level is 2 then knight moves differently, due to game rules.
-		if(level == 2)
-		{
+		int numberOfPossibleSquares;
+		
+		if (possibleNewRow.length == Constants.SQUARES_LEVEL_TWO)
 			numberOfPossibleSquares = Constants.SQUARES_LEVEL_TWO;
-			possibleNewRow = possibleRowMovesLevelTwo();
-			possibleNewCol = possibleColMovesLevelTwo();
-		}
-		// in other levels the knight moves regularly 
-		else
-		{
-			possibleNewRow = possibleRowMovesLevelDefault();
-			possibleNewCol = possibleColMovesLevelDefault();
-		}
-
+		else 
+			numberOfPossibleSquares = Constants.SQUARES_DEFAULT;
+	
+		//fix indexes that are out of bound
 		possibleNewRow = minusTurnsToPlusLocation(possibleNewRow);
 		possibleNewCol = minusTurnsToPlusLocation(possibleNewCol);
 		
-		// now we have two arrays, each of them has locations of the placements, first array is the Row and the second is the Column, now we will check if the place is empty
+		//now we have two arrays, each of them has locations of the placements, first array
+		//is the Row and the second is the Column, we will match them to create a list of all possible squares
 		for(int i=0;i<numberOfPossibleSquares;i++) {
 			possibleMoves[possibleNewRow[i]][possibleNewCol[i]] = new Square("noColor", false);
 			if((possibleNewRow[i] >=0 && possibleNewRow[i] <8 && possibleNewCol[i] >=0 && possibleNewCol[i]<8))
@@ -46,63 +81,7 @@ public class Knight extends Piece{
 		}
 		
 		return possibleMoves;
-
-	}
-
-	// possible row moves according to chess regular rules
-	public int[] possibleRowMovesLevelDefault() {
-		int currentRow = getRow();
-		int possibleNewRow[] = {currentRow-1,currentRow+1,currentRow-2,currentRow+2,currentRow-2,
-				currentRow+2,currentRow-1,currentRow+1};
-		return possibleNewRow;
-	}
-	
-	// possible column moves according to chess regular rules
-	public int[] possibleColMovesLevelDefault() {
-		int currentCol = getCol();
-		int possibleNewCol[] = {currentCol-2,currentCol-2,currentCol-1,currentCol-1,currentCol+1,
-				currentCol+1,currentCol+2,currentCol+2};
-		return possibleNewCol;
-	}
-
-	//possible row moves according to level 2 rules
-	//two squares straight then one diagonal or two squares diagonal then one straight
-	public int[] possibleRowMovesLevelTwo() {
-		int currentRow = getRow();
-		int possibleNewRow[] = {currentRow-2,currentRow-1,currentRow+1,currentRow+2,currentRow-3,currentRow+3,
-				currentRow-3,currentRow+3,currentRow-3,currentRow+3,currentRow-3,currentRow+3,
-				currentRow-2,currentRow-1,currentRow+1,currentRow+2};
-	
-		return possibleNewRow;
-	}
-	
-	//possible column moves according to level 2 rules
-	//two squares straight then one diagonal or two squares diagonal then one straight
-	public int[] possibleColMovesLevelTwo() {		
-		int currentCol = getCol();
-		int possibleNewCol[] = {currentCol-3,currentCol-3,currentCol-3,currentCol-3,currentCol-2,currentCol-2,
-				currentCol-1,currentCol-1,currentCol+1,currentCol+1,currentCol+2,currentCol+2,
-				currentCol+3,currentCol+3,currentCol+3,currentCol+3};
 		
-		return possibleNewCol;
 	}
-
-	// method in case the knight wants to go from one side of the board to the other
-	public int[] minusTurnsToPlusLocation(int possibleLoc[])
-	{
-		for(int i=0; i<possibleLoc.length; i++)
-		{
-			if(possibleLoc[i] < 0)
-			{
-				possibleLoc[i] = 8 + possibleLoc[i];
-			}
-			if (possibleLoc[i] > 7) 
-			{
-				possibleLoc[i] = possibleLoc[i] - 8;
-			}
-		}
-		
-		return possibleLoc;
-	}
-
+	
 }
