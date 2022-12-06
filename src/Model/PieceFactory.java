e9f4741d07d97bcc208c1df72ed6a14017c7364e
matan417 <matan417@gmail.com>
2022-12-06 12:38:13 +0200

    create PieceFactory
    starting Design Pattern - Factory Pattern

---
+++ src/Model/PieceFactory.java
@@ -0,0 +1,22 @@
+package Model;
+
+public class PieceFactory 
+{
+	public Piece getPiece(String peiceType, int row, int col)
+	{
+	      if(peiceType == null){
+	          return null;
+	       }		
+	       if(peiceType.equalsIgnoreCase("KING")){
+	          return new King(row,col);
+	          
+	       } else if(peiceType.equalsIgnoreCase("KNIGHT")){
+	          return new Knight(row,col);
+	          
+	       } else if(peiceType.equalsIgnoreCase("QUEEN")){
+	          return new Queen(row,col);
+	       }
+	       return null;
+	}
+
+}
