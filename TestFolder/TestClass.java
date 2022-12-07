322150b2440db8a61e44df89beee3fee930aaa0c
matan417 <matan417@gmail.com>
2022-12-07 10:04:25 +0200

    testing example Junit

---
+++ TestFolder/TestClass.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * 
+ */
+
+/**
+ * @author Matan
+ *
+ */
+class TestClass {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
