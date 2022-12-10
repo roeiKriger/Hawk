d237837618c0ecfad31aecee6bd6ec542d6ad449
Liad Uziel <liad.uz@gmail.com>
2022-12-10 12:28:57 +0200

    save selected question for edit  / remove options
    in addition, disable the edit/delete buttons when question wasn't selected.

--- src/Control/ViewLogic/QuestionEditor.java
+++ src/Control/ViewLogic/QuestionEditor.java
@@ -4,8 +4,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-import org.json.simple.parser.ParseException;
-
 import Control.SysData;
 import Model.Question;
 import Utils.Difficulty;
@@ -16,8 +14,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.AnchorPane;
@@ -46,6 +46,13 @@ public class QuestionEditor implements Initializable {
 	public TableColumn<Question, String> cont;// text of the question
 	@FXML
 	public TableColumn<Question, String> correct; // correct answer
+	
+	
+    @FXML
+    private Button editBtn;
+    @FXML
+    private Button deleteBtn;
+	
 
 	private SysData sd = SysData.getInstance();
 	private ObservableList<Question> questions;
@@ -69,6 +76,33 @@ public class QuestionEditor implements Initializable {
 			cont.setCellValueFactory(new PropertyValueFactory<>("questionContent"));
 			correct.setCellValueFactory(new PropertyValueFactory<>("correctAnswerId"));
 			questionsTable.setItems(questions);
+			
+			editBtn.setDisable(true);
+			deleteBtn.setDisable(true);
+			
+			// event listener that save the selected question
+			questionsTable.setRowFactory(ev -> {
+				return rowSelectedHandler();
+			});
+	}
+	
+	/*
+	 * event listener of selected row in table
+	 * the function saves the selected question in sysdata 
+	 */
+	private TableRow<Question> rowSelectedHandler() {
+		TableRow<Question> rowSelectedQuestion = new TableRow<>();
+		
+		rowSelectedQuestion.setOnMousePressed(eventClicked -> {
+			 if (eventClicked.isPrimaryButtonDown() && !rowSelectedQuestion.isEmpty()) {
+		            // get the selected item and save this in sys data
+		            sd.setEditedQuestion(rowSelectedQuestion.getItem());
+		            
+		            editBtn.setDisable(false);
+		            deleteBtn.setDisable(false);
+		        }
+		});
+		return rowSelectedQuestion;
 	}
 	
 	/*
@@ -85,22 +119,25 @@ public class QuestionEditor implements Initializable {
 			questionsTable.setItems(FXCollections.observableArrayList(sd.getQuestionsLevel3()));	
     }
 	
+	/*
+	 *	function that change addEditFlag and move to add screen 
+	 */
     @FXML
     void onAddQuestion(ActionEvent event) throws IOException {
     	// change addEditFlag in sysdata
     	sd.setAddEditFlag("add");
     	
     	moveToAddEditScreen();
-
     }
 
+    /*
+     * function that change addEditFlag and move to edit screen 
+     */
     @FXML
     void onEditQuestion(ActionEvent event) throws IOException {
     	// change addEditFlag in sysdata
     	sd.setAddEditFlag("edit");
     	
-    	// save question in sysdata
-    	sd.setEditedQuestion(sd.get_questions().get(0)); // TODO - change this to selected question
     	moveToAddEditScreen();
     }
     
