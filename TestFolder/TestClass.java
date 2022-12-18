0fd9b98398ac680524600c154ca95fe15d2e807e
matan417 <matan417@gmail.com>
2022-12-18 12:36:30 +0200

    adding Junit test of adding new Question

--- TestFolder/TestClass.java
+++ TestFolder/TestClass.java
@@ -1,12 +1,19 @@
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import org.json.simple.parser.ParseException;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import Control.SysData;
 import Model.Game;
+import Model.Question;
 
 /**
  * 
@@ -18,8 +25,10 @@ import Model.Game;
  */
 class TestClass 
 {
+	private List<String> answers;
+	private Question testQuestion;
 	/*
-	 * This test is testing if game is inisilaize in score of 0
+	 * This test is testing if game is initialize in score of 0
 	 */
 	@Test
 	public void testScoreInit()
@@ -30,7 +39,7 @@ class TestClass
 	}
 	
 	/*
-	 * This test is testing if the Json File is read successfully
+	 * This test is testing if the Question JSON File is read successfully
 	 */
 	@Test
 	public void testReadQuestionsJson() throws IOException, ParseException
@@ -39,8 +48,41 @@ class TestClass
 	}
 	
 	/*
-	 * 
+	 * This test is testing if the Scores JSON File is read successfully
 	 */
+	@Test
+	public void testReadScoresJson() throws java.text.ParseException {
+		assertTrue(SysData.getInstance().import_scores());
+	}
 	
-	
+	/*
+	 * This method trying to add new Question 
+	 * Testing if just one of the answers is correct
+	 * if the question content is not null
+	 * check if difficulty is correct
+	 */
+	@Before
+	public List<String> initializeAnswers()
+	{
+		answers = new ArrayList<String>();
+		answers.add("A");
+		answers.add("B");
+		answers.add("C");
+		answers.add("D");
+		return answers;
+	}
+
+	@Test
+	public void addQuestionTest() throws IOException
+	{
+		testQuestion = new Question(1, "Whay you prefer?", initializeAnswers(), 1);
+		assertNotNull(testQuestion.getQuestionContent());
+		assertEquals(1, testQuestion.getQuestionDifficulty());
+		assertTrue(testQuestion.checkCorrectAnswer("A"));
+		assertFalse(testQuestion.checkCorrectAnswer("B"));
+		assertFalse(testQuestion.checkCorrectAnswer("C"));
+		assertFalse(testQuestion.checkCorrectAnswer("D"));
+	}
+
+
 }
