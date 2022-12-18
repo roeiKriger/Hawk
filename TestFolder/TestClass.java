4194d7ac3a03f77fa25903eb0c2c4834726788f0
matan417 <matan417@gmail.com>
2022-12-18 11:39:24 +0200

    adding Junit of inisliazie score

--- TestFolder/TestClass.java
+++ TestFolder/TestClass.java
@@ -2,6 +2,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import Model.Game;
+
 /**
  * 
  */
@@ -15,6 +17,18 @@ class TestClass {
 	@Test
 	void test() {
 		fail("Not yet implemented");
+	}
+	
+	/*
+	 * This test is testing if game is inisilaize in score of 0
+	 */
+	@Test
+	public void testScoreInit()
+	{
+		Game g = new Game("jUnit");
+		int score_actual = g.getScore();
+		assertEquals(0, score_actual);
+		
 	}
 
 }
