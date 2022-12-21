297223c27aa527ff0ce899676a3690a54b693f80
nogadm <nogadm@gmail.com>
2022-12-21 17:40:42 +0200

    added initial knight location junit test

--- TestFolder/TestClass.java
+++ TestFolder/TestClass.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Date;
 import java.util.List;
 
 import org.json.simple.parser.ParseException;
@@ -13,27 +14,43 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import Control.SysData;
+import Exceptions.JsonException;
 import Model.Game;
+import Model.Knight;
 import Model.Question;
+import Model.Square;
+import javafx.scene.control.Alert.AlertType;
 
 class TestClass 
 {
 	
 	private static List<String> answers = new ArrayList<String>() ;
 	private static Question testQuestion;
-	private Game g;
+	private static Game g;
+	private static SysData sd;
 	
 	/*
 	 * Before all testing initialize new Question
 	 */
 	@BeforeAll
-	public static void initializeQuestion()
+	public static void initializeQuestionAndGame() throws IOException, ParseException
 	{
 		answers.add("A");
 		answers.add("B");
 		answers.add("C");
 		answers.add("D");
-		testQuestion = new Question(1, "Whay you prefer?", answers, 1);
+		testQuestion = new Question(1, "What do you prefer?", answers, 1);
+		
+		
+		sd = SysData.getInstance();
+		try {
+		if (!sd.load_questions()) // import not successful
+			throw new JsonException();
+		} catch (JsonException e) {
+			SysData.alert(e.getMessage(), e.getMessage(), AlertType.ERROR);
+		}
+		sd.setGame(new Game("jUnit", new Date()));
+		g = sd.getGame();
 	}
 	
 	
@@ -77,6 +94,34 @@ class TestClass
 		assertFalse(testQuestion.checkCorrectAnswer("C"));
 		assertFalse(testQuestion.checkCorrectAnswer("D"));
 	}
+	
+	
+	/*
+	 * Testing the knight starts at 0,0 tile in the beginning of each level
+	 */
+	@Test
+	public void testKnightInitialLocation() throws IOException, ParseException
+	{			
+
+		g.createBoardLevelOne();
+		assertEquals(g.getKnight().getRow(), 0);
+		assertEquals(g.getKnight().getCol(), 0);
+		
+		g.createBoardLevelTwo();
+		assertEquals(g.getKnight().getRow(), 0);
+		assertEquals(g.getKnight().getCol(), 0);
+		
+		g.createBoardLevelThree();
+		assertEquals(g.getKnight().getRow(), 0);
+		assertEquals(g.getKnight().getCol(), 0);
+		
+		g.createBoardLevelFour();
+		assertEquals(g.getKnight().getRow(), 0);
+		assertEquals(g.getKnight().getCol(), 0);
+	
+	}
+	
+	
 	
 
 }
